4a923bb69fb20b3cdf6a8dce2386b191172f6aca
dualface <dualface@gmail.com>
2013-03-13 02:21:26 +0800

    update proj.android

--- cocos2dx/Android.mk
+++ cocos2dx/Android.mk
@@ -68,6 +68,7 @@ layers_scenes_transitions_nodes/CCTransitionProgress.cpp \
 menu_nodes/CCMenu.cpp \
 menu_nodes/CCMenuItem.cpp \
 misc_nodes/CCClippingNode.cpp \
+misc_nodes/CCClippingRegionNode.cpp \
 misc_nodes/CCMotionStreak.cpp \
 misc_nodes/CCProgressTimer.cpp \
 misc_nodes/CCRenderTexture.cpp \
