61500bc4e1e9af26f0687cf613888a1b808f2926
dualface <dualface@gmail.com>
2013-05-25 15:50:36 +0800

    fix compile warnings

--- CocosDenshion/include/SimpleAudioEngine.h
+++ CocosDenshion/include/SimpleAudioEngine.h
@@ -31,6 +31,7 @@ THE SOFTWARE.
 #include <typeinfo>
 #include <ctype.h>
 #include <string.h>
+#include "CCPlatformDefine.h"
 
 namespace CocosDenshion {
 
@@ -59,7 +60,7 @@ static inline unsigned int getHashCodeByString(const char *key)
 @brief          offer a VERY simple interface to play background music & sound effect
 */
 
-class EXPORT_DLL SimpleAudioEngine : public TypeInfo
+class CC_DLL SimpleAudioEngine : public TypeInfo
 {
 public:
     SimpleAudioEngine();
